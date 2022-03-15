d6fa046cf5cd77e609b1dc625d669895607cf50a
GrischK <grischka.gorski@laposte.net>
2022-03-15 15:19:17 +0100

    modifs carrousel

--- carrousel_button.js
+++ carrousel_button.js
@@ -1,11 +1,11 @@
-document.body.onload = function() {
+const imgSlider = ["./assets/Photos-chats/Beebop.jpg", "./assets/Photos-chats/Bella.jpg","./assets/Photos-chats/Bossa.jpeg", "./assets/Photos-chats/Chacha.jpg", "./assets/Photos-chats/Charly.jpg","./assets/Photos-chats/Chatons.jpg", "./assets/Photos-chats/Daisy.jpg", "./assets/Photos-chats/Dominika.jpg", "./assets/Photos-chats/Donatello.jpg", "./assets/Photos-chats/Koshka.jpg", "./assets/Photos-chats/Leonardo.jpg", "./assets/Photos-chats/Luigi.jpg", "./assets/Photos-chats/Mario.jpg", "./assets/Photos-chats/Michelangelo.jpg", "./assets/Photos-chats/Nova.jpg", "./assets/Photos-chats/Raphaello.jpg", "./assets/Photos-chats/Sacha.jpg", "./assets/Photos-chats/Samba.jpg", "./assets/Photos-chats/Spoutnik.jpg", "./assets/Photos-chats/Sveta.jpg", "./assets/Photos-chats/Warrio.jpg",];
+let container = document.body.querySelector(".carrousel-container");
+let leftBtn = document.querySelector(".btn-g");
+let rightBtn = document.querySelector(".btn-d");
+let nbr = imgSlider.length;
+let i = 0;
 
-    const imgSlider = ["./assets/Photos-chats/Beebop.jpg", "./assets/Photos-chats/Bella.jpg","./assets/Photos-chats/Bossa.jpeg", "./assets/Photos-chats/Chacha.jpg", "./assets/Photos-chats/Charly.jpg","./assets/Photos-chats/Chatons.jpg", "./assets/Photos-chats/Daisy.jpg", "./assets/Photos-chats/Dominika.jpg", "./assets/Photos-chats/Donatello.jpg", "./assets/Photos-chats/Koshka.jpg", "./assets/Photos-chats/Leonardo.jpg", "./assets/Photos-chats/Luigi.jpg", "./assets/Photos-chats/Mario.jpg", "./assets/Photos-chats/Michelangelo.jpg", "./assets/Photos-chats/Nova.jpg", "./assets/Photos-chats/Raphaello.jpg", "./assets/Photos-chats/Sacha.jpg", "./assets/Photos-chats/Samba.jpg", "./assets/Photos-chats/Spoutnik.jpg", "./assets/Photos-chats/Sveta.jpg", "./assets/Photos-chats/Warrio.jpg",];
-    container = document.body.querySelector(".carrousel-container");
-    g = document.querySelector(".btn-g");
-    d = document.querySelector(".btn-d");
-    nbr = imgSlider.length;
-    i = 0;
+document.body.onload = function() {
 
     //taille du container avec toutes les images
 
@@ -21,6 +21,7 @@ document.body.onload = function() {
     }
 }
 
-g.onclick = function(){
+leftBtn.onclick = function(){
     i--;
+    container.style.transform = "translate ("+ i * 800 +"px)";
 }
