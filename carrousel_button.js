7ccceda2501dcdd8ef1fa20e6aeaddb4998906e4
GrischK <grischka.gorski@laposte.net>
2022-03-15 12:19:40 +0100

    ajout nouveau carrousel test avec bouton

---
+++ carrousel_button.js
@@ -0,0 +1,21 @@
+document.body.onload = function() {
+
+    const imgSlider = ["./assets/Photos-chats/Beebop.jpg", "./assets/Photos-chats/Bella.jpg","./assets/Photos-chats/Bossa.jpeg", "./assets/Photos-chats/Chacha.jpg", "./assets/Photos-chats/Charly.jpg","./assets/Photos-chats/Chatons.jpg", "./assets/Photos-chats/Daisy.jpg", "./assets/Photos-chats/Dominika.jpg", "./assets/Photos-chats/Donatello.jpg", "./assets/Photos-chats/Koshka.jpg", "./assets/Photos-chats/Leonardo.jpg", "./assets/Photos-chats/Luigi.jpg", "./assets/Photos-chats/Mario.jpg", "./assets/Photos-chats/Michelangelo.jpg", "./assets/Photos-chats/Nova.jpg", "./assets/Photos-chats/Raphaello.jpg", "./assets/Photos-chats/Sacha.jpg", "./assets/Photos-chats/Samba.jpg", "./assets/Photos-chats/Spoutnik.jpg", "./assets/Photos-chats/Sveta.jpg", "./assets/Photos-chats/Warrio.jpg",];
+    container = document.body.querySelector(".carrousel");
+    g = document.querySelector(".btn-g");
+    d = document.querySelector(".btn-d");
+    nbr = imgSlider.length;
+
+    //taille du container avec toutes les images
+
+    container.style.width=(100 * nbr) + "%";
+
+    //ajout des images dans la div qui se créé dans le carrousel
+
+    for (let i = 0; i < nbr; i++) {
+        div = document.createElement("div");
+        div.className = "photo";
+        div.style.backgroundImage = "url('./assets/Photos-chats/img"+i+".jpg')";
+        container.appendChild(div);
+    }
+}
