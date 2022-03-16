939266b418f699975bb4d52e4c426413d69fce88
GrischK <grischka.gorski@laposte.net>
2022-03-16 15:03:35 +0100

    fonctionnalités carrousel

--- carrousel_button.js
+++ carrousel_button.js
@@ -1,46 +1,48 @@
-const imgSlider = [
-  "./assets/Photos-chats/Beebop.jpg",
-  "./assets/Photos-chats/Bella.jpg",
-  "./assets/Photos-chats/Bossa.jpeg",
-  "./assets/Photos-chats/Chacha.jpg",
-  "./assets/Photos-chats/Charly.jpg",
-  "./assets/Photos-chats/Chatons.jpg",
-  "./assets/Photos-chats/Daisy.jpg",
-  "./assets/Photos-chats/Dominika.jpg",
-  "./assets/Photos-chats/Donatello.jpg",
-  "./assets/Photos-chats/Koshka.jpg",
-  "./assets/Photos-chats/Leonardo.jpg",
-  "./assets/Photos-chats/Luigi.jpg",
-  "./assets/Photos-chats/Mario.jpg",
-  "./assets/Photos-chats/Michelangelo.jpg",
-  "./assets/Photos-chats/Nova.jpg",
-  "./assets/Photos-chats/Raphaello.jpg",
-  "./assets/Photos-chats/Sacha.jpg",
-  "./assets/Photos-chats/Samba.jpg",
-  "./assets/Photos-chats/Spoutnik.jpg",
-  "./assets/Photos-chats/Sveta.jpg",
-  "./assets/Photos-chats/Warrio.jpg",
+const catList = [
+  { name: "chat 1", race: "européen" },
+  { name: "chat 2", race: "bengal" },
+  { name: "chat 3", race: "siamois" },
+  { name: "chat 4", race: "chartreux" },
+  { name: "chat 5", race: "bengal" },
+  { name: "chat 6", race: "chartreux" },
+  { name: "chat 7", race: "bengal" },
 ];
+
 const container = document.body.querySelector(".carrousel-container");
 const leftBtn = document.querySelector(".btn-g");
 const rightBtn = document.querySelector(".btn-d");
 let sliderPosition = 0;
-let nbr = imgSlider.length;
 
-document.body.onload = function () {
-  //taille du container avec toutes les images
+//taille du container avec toutes les images
+container.style.width = 90 * catList.length + "vw";
 
-  container.style.width = 800 * nbr + "px";
+//ajout des images dans la div qui se créé dans le carrousel
 
-  //ajout des images dans la div qui se créé dans le carrousel
+for (i = 0; i < catList.length; i++) {
+  const cat = catList[i];
 
-  for (i = 0; i < nbr; i++) {
-    div = document.createElement("div");
-    div.className = "photo";
-    div.style.backgroundImage = "url('./assets/Photos-chats/img" + i + ".jpg')";
-    container.appendChild(div);
-  }
-};
+  const div = document.createElement("div");
+  div.className = "photo";
+  div.style.backgroundImage = "url('./assets/Photos-chats/img" + i + ".jpg')";
+
+  // On attache les données du chat à l'élément div
+  div.dataset.name = cat.name;
+  div.dataset.race = cat.race;
+
+  div.addEventListener("click", function (event) {
+    // event.target contient un référence vers l'élément du DOM
+    // qui a lancé le click
+    console.log(event.target.dataset);
+    event.target.classList.toggle('visible');
+  });
+
+  // Création des informations du chat
+  const infos = document.createElement("p");
+  infos.innerHTML = cat.name;
+  div.appendChild(infos);
+
+  container.appendChild(div);
+}
 
 leftBtn.addEventListener("click", function () {
   slide(1);
@@ -52,11 +54,16 @@ rightBtn.addEventListener("click", function () {
 
 function slide(direction = 1) {
   if (direction === 1 && sliderPosition === 0) {
-      return;
-  } else if (direction === -1 && sliderPosition === -16000) {
-      return;
+    return;
   }
 
-  sliderPosition += 800 * direction;
-  container.style.transform = `translateX(${sliderPosition}px)`;
+  if (
+    direction === -1 &&
+    sliderPosition <= (catList.length - 1) * 90 * direction
+  ) {
+    return;
+  }
+
+  sliderPosition += 90 * direction;
+  container.style.transform = `translateX(${sliderPosition}vw)`;
 }
