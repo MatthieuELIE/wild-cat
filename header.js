f67a1a316e77c5252ce1a3c0f58a6b41fc608ab2
Babongui mabika venuza <vhenuzya@yahoo.fr>
2022-03-22 09:43:52 +0100

    update header, footer and section soutien ton asso

--- header.js
+++ header.js
@@ -1,7 +1,26 @@
-document.querySelectorAll(".menu__item ").forEach(function(element){
-    element.addEventListener("click", function () {
-        document.querySelector("#menu__toggle").checked=false;
-      });
-    });
 
-    
+
+    // header2
+
+    function toggleMenu () {  
+      const navbar = document.querySelector('.navbar');
+      const burger = document.querySelector('.burger');
+      burger.addEventListener('click', (e) => {    
+        navbar.classList.toggle('show-nav');
+      });    
+    }
+    toggleMenu();
+  
+
+
+    1
+    2
+    3
+
+
+    document.querySelectorAll(".navbar__links").forEach(function(element){
+      element.addEventListener("click", function () {
+      document.querySelector(".navbar").click();
+        });
+    });
+ 
